f085bef539d098acf321d00e3d4861c09e5e0478
tmtz <metzias@t-online.de>
2021-02-07 12:46:27 +0100

    init main_state.rs

---
+++ src/main_state.rs
@@ -0,0 +1,10 @@
+use orbtk::prelude::*;
+
+#[derive(Default, AsAny)]
+pub struct MainState {
+}
+
+impl State for MainState {
+    fn update(&mut self, _: &mut Registry, _: &mut Context) {
+    }
+}
